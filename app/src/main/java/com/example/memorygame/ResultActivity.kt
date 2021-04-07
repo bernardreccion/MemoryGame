18e2af8c3759807b0e71914bc693c4689cdbdc83
bernardreccion <bernardguiller.reccion@benilde.edu.ph>
2021-04-07 17:24:36 +0800

    add function for share icon

--- app/src/main/java/com/example/memorygame/ResultActivity.kt
+++ app/src/main/java/com/example/memorygame/ResultActivity.kt
@@ -2,10 +2,13 @@ package com.example.memorygame
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Chronometer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.memorygame.databinding.ActivityResultBinding
+import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
@@ -14,6 +17,7 @@ class ResultActivity : AppCompatActivity() {
     private lateinit var chronometer: Chronometer
     private var backPressedTime: Long = 0
     private var timeFinished: Long = 0
+    private lateinit var score: String
     private val db = DatabaseHandler(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +32,7 @@ class ResultActivity : AppCompatActivity() {
 
 
         //string format to display 00:00 for better readability (from Long to String format)
-        val score = String.format("%02d:%02d",
+        score = String.format("%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(timeFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(timeFinished)),
                 TimeUnit.MILLISECONDS.toSeconds(timeFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(timeFinished)))
 
@@ -82,6 +86,25 @@ class ResultActivity : AppCompatActivity() {
                 break
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_result, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.itemShare) {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "LoL Champions: Memory Game: \n You have finished the game at $score on ${Calendar.getInstance().time}")
+
+                type = "text/plain"
+            }
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
